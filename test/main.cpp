bda91af6ad705242a60b7cd8d81b68db746c9dfb
Maggie-Mira <maggie890808@gmail.com>
2020-03-17 10:45:14 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 吳詠涵 on 2020/3/17.
+//  Copyright © 2020 吳詠涵. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
